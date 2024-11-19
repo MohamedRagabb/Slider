6a599e7917d76bc11d36e48c6c95794f7c700985
MohamedRagabb <mohamedbksaftawy@gmail.com>
2024-11-19 10:51:01 +0200

    update

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,40 @@
-console.log("efmlqe");
+var imgList = Array.from(document.querySelectorAll('.item img'));
+var lightBoxContainer = document.querySelector('.lightBoxContainer')
+var lightitem = document.querySelector('.lightitem')
+
+var nextBtn = document.querySelector('#next');
+var prvBtn = document.querySelector('#prv');
+var currentIndex;
+
+for(let i=0 ; i<imgList.length ; i++){
+    imgList[i].addEventListener('click' , function(event){
+        lightBoxContainer.classList.replace('d-none' , 'd-flex')
+        var imgsrc = event.target.getAttribute('src')
+        lightitem.style.backgroundImage = `url(${imgsrc})`;
+        currentIndex = imgList.indexOf(event.target) ;
+        console.log();
+        
+    })
+}
+
+
+function nextSlide(){
+    if (currentIndex== imgList.length){
+        currentIndex=0;
+    }
+    currentIndex++;
+    var imgsrc = imgList[currentIndex].getAttribute('src')
+    lightitem.style.backgroundImage = `url(${imgsrc})`
+    
+    
+}
+
+function prvSilde(){
+    console.log (
+        "hello "
+    )
+
+}
+
+prvBtn.addEventListener('click' , prvSilde)
+nextBtn.addEventListener('click' , nextSlide)
